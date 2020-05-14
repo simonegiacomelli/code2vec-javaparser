d89863e0b282cf2eed1947b912a0e42f08a9b07e
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-05-14 16:36:17 +0200

    development of JavaExtractor preprocessing

--- src/main/java/JavaExtractor/ExtractFeaturesTask.java
+++ src/main/java/JavaExtractor/ExtractFeaturesTask.java
@@ -10,7 +10,6 @@ import java.util.concurrent.Callable;
 import org.apache.commons.lang3.StringUtils;
 
 import com.github.javaparser.ParseException;
-import com.github.javaparser.ast.CompilationUnit;
 
 import JavaExtractor.Common.CommandLineValues;
 import JavaExtractor.Common.Common;
@@ -50,7 +49,7 @@ public class ExtractFeaturesTask implements Callable<Void> {
             return;
         }
 
-        String toPrint = featuresToString(features);
+        String toPrint = featuresToString(features, m_CommandLineValues);
         if (toPrint.length() > 0) {
             System.out.println(fn + "OK\t" + toPrint);
 //            System.out.println(toPrint);
@@ -73,7 +72,7 @@ public class ExtractFeaturesTask implements Callable<Void> {
         return features;
     }
 
-    public String featuresToString(ArrayList<ProgramFeatures> features) {
+    public static String featuresToString(ArrayList<ProgramFeatures> features, CommandLineValues m_commandLineValues) {
         if (features == null || features.isEmpty()) {
             return Common.EmptyString;
         }
@@ -85,7 +84,7 @@ public class ExtractFeaturesTask implements Callable<Void> {
 
             String toPrint = Common.EmptyString;
             toPrint = singleMethodfeatures.toString();
-            if (m_CommandLineValues.PrettyPrint) {
+            if (m_commandLineValues.PrettyPrint) {
                 toPrint = toPrint.replace(" ", "\n\t");
             }
             builder.append(toPrint);
--- src/main/java/JavaExtractor/SocketServer/App.kt
+++ src/main/java/JavaExtractor/SocketServer/App.kt
@@ -1,9 +1,22 @@
 package JavaExtractor.SocketServer
 
+import JavaExtractor.Common.CommandLineValues
+import JavaExtractor.ExtractFeaturesTask
+import JavaExtractor.FeatureExtractor
+import JavaExtractor.FeaturesEntities.ProgramFeatures
 import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.lang.Exception
 import java.net.ServerSocket
 import java.net.Socket
+import java.util.ArrayList
 import kotlin.concurrent.thread
+
+private val args = CommandLineValues(
+    *"--max_path_length 8 --max_path_width 2 --dir ../code2vec-satd/build-dataset/java-small/one --num_threads 10".split(
+        " "
+    ).toTypedArray()
+)
 
 fun main() {
     val server = ServerSocket(9999)
@@ -15,15 +28,55 @@ fun main() {
 }
 
 class ClientHandler(val client: Socket) {
+    val out = DataOutputStream(client.getOutputStream())
     fun serve() {
 
-        client.getInputStream().bufferedReader().use { buffer ->
-            val lineCount = buffer.readLine().orEmpty().toIntOrNull() ?: 0
-            println("lineCount=$lineCount")
-            val lines = (0..lineCount).map { buffer.readLine().orEmpty() }
-            println(lines.joinToString("\n"))
+        DataInputStream(client.getInputStream()).use { inp ->
+            val str = try {
+                val code = inp.readChunkedString()
+//                println("------------------------------------")
+//                println(code)
+                val featureExtractor = FeatureExtractor(args)
+
+                val features = featureExtractor.extractFeatures(code)
+                featuresToString(features)
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+                "FAILED\t"+ ex.toString().replace("\n", "\\n")
+            }
+            try {
+                out.sendStringInChunk(str)
+            } catch (e: Exception) {
+            }
+            try {
+                out.close()
+            } catch (e: Exception) {
+            }
         }
     }
 
+    private fun featuresToString(features: ArrayList<ProgramFeatures>?) = if (features == null) {
+        "FAILED\tNULL"
+    } else {
+        val toPrint = ExtractFeaturesTask.featuresToString(features, args)
+        if (toPrint.isNotEmpty()) {
+            "OK\t$toPrint"
+        } else "FAILED\ttoPrint.length()==0"
+    }
 
+
+}
+
+
+private fun DataOutputStream.sendStringInChunk(source: String) {
+    val chunk = source.chunked(1024 * 32)
+    writeInt(chunk.size)
+    chunk.forEach { writeUTF(it) }
+    flush()
+}
+
+private fun DataInputStream.readChunkedString(): String {
+    val chunkCount = readInt()
+    val code = (1..chunkCount).joinToString("") { readUTF() }
+    return code
 }
